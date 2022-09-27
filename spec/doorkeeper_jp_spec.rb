a127d0db6f8870e267cde53bd8a2bc0707c83af0
sue445 <sue445@sue445.net>
2022-09-27 22:36:11 +0900

    Remove skeleton examples

--- spec/doorkeeper_jp_spec.rb
+++ spec/doorkeeper_jp_spec.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 RSpec.describe DoorkeeperJp do
-  it "has a version number" do
-    expect(DoorkeeperJp::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
