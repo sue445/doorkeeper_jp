e7b0e667643d4ff60d4a386992ecad52894f0d9f
sue445 <sue445@sue445.net>
2025-04-06 10:27:04 +0900

    Tell inheritis to yard

--- lib/doorkeeper_jp/response.rb
+++ lib/doorkeeper_jp/response.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
+# @!parse
+#   module Hashie
+#     # @see https://www.rubydoc.info/gems/hashie/Hashie/Hash
+#     class Hash < ::Hash
+#     end
+#
+#     # @see https://www.rubydoc.info/gems/hashie/Hashie/Mash
+#     class Mash < Hash
+#     end
+#   end
+
 module DoorkeeperJp
   require "hashie/mash"
 
+  # Doorkeeper API response
+  #
+  # @see https://www.rubydoc.info/gems/hashie/Hashie/Hash
+  # @see https://www.rubydoc.info/gems/hashie/Hashie/Mash
   class Response < Hashie::Mash
     disable_warnings
   end
